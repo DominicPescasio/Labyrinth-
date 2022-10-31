2616bdc634491e19beac7a8e60a5bdf4a92b26dc
DominicPescasio <DomPescasio@gmail.com>
2022-10-31 12:49:45 -0700

    Fixed the Win screen

--- Labyrinth/Assets/GameScrpts/PlayerScripts/PlayerHealth.cs
+++ Labyrinth/Assets/GameScrpts/PlayerScripts/PlayerHealth.cs
@@ -18,7 +18,7 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.F4))
         {
             TakeDamage(1);
         }
